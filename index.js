f4e60f868c3dd6e4bfd34831c66be48e266c774b
Noah Grant <1355779+noahgrant@users.noreply.github.com>
2021-07-26 09:55:24 -0700

    expose ajax method from sync module (#21)

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ export {setAjaxPrefilter} from './lib/config.js';
 export {default as Events} from './lib/events.js';
 export {default as History} from './lib/history.js';
 export {default as Model} from './lib/model.js';
-export {default as sync} from './lib/sync.js';
+export {default as sync, ajax} from './lib/sync.js';
 export {default as Router} from './lib/router.js';
 export {history} from './lib/history.js';
